8edaca3535bb0ce0bd92e7ba67f28276e45f63d5
Jack Sipley <69880845+jrs8215@users.noreply.github.com>
2021-12-29 21:18:03 -0500

    Define pinout for arduino to LED circuit

--- Snake/Snake.ino
+++ Snake/Snake.ino
@@ -1,3 +1,24 @@
+#define ROW_1 2
+#define ROW_2 3
+#define ROW_3 4
+#define ROW_4 5
+#define ROW_5 6
+#define ROW_6 7
+#define ROW_7 8
+#define ROW_8 9
+
+#define COL_1 10
+#define COL_2 11
+#define COL_3 12
+#define COL_4 A0
+#define COL_5 A1
+#define COL_6 A2
+#define COL_7 A3
+#define COL_8 A4
+
+const byte row[] = { ROW_1, ROW_2, ROW_3, ROW_4, ROW_5, ROW_6, ROW_7, ROW_8 };
+const byte col[] = { COL_1,COL_2, COL_3, COL_4, COL_5, COL_6, COL_7, COL_8 };
+
 void setup() {
   // put your setup code here, to run once:
 
